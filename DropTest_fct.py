c543a8d38fa96cfebb1d4630344f48ba8a48e4f5
chromatimera <popescuiofelia@gmail.com>
2023-03-17 17:54:45 +0100

    Nf Ni vs partition factor (Not working)
    started working on the loop for plotting Nf, Ni versus partition but for now it does not work.

--- DropTest_fct.py
+++ DropTest_fct.py
@@ -108,7 +108,7 @@ class DropTest(object):
             pd.DataFrame(new).to_csv('output/df_growth_{}_count_survival_nr_drop_{}_ab_range_{}_{}.csv'.format(growth,total_drop_nr,abmin,abmax), index= None)
         return new
 
-    def test_surv_frac_diff_partitioning(self, partmin, partmax, step):
+    def test_surv_frac_diff_partitioning(self, partmin, partmax, step): ## one AB conc
         new = pd.DataFrame()
 
         # loop for partitioning factors
@@ -122,23 +122,14 @@ class DropTest(object):
                 Droplet_exp = droplets_R(total_drop_nr, strain_R, AB_conc, new_volume)  # 0.5, 300
                 Droplet_exp.run(loading, growth)
                 Droplet_exp.countSurvival(growth)
-                #surv_frac = Droplet_exp.Res_survival_fraction
-                #print(surv_frac)
                 additional = pd.DataFrame({"" + str(new_nr_drops_total_mass) + "": [Droplet_exp.Res_survival_fraction]})
                 # print(additional)
                 new = pd.concat([new, additional], axis=1)
                 #Droplet_exp.plots(growth)
-       #         Droplet_exp.save('initialN{}_growthrate{}_MIC{}_totaldropnr{}_vol{}_loading{}_growth{}.csv'.format(initialN,
-       #                                                                                                        growthrate,
-       #                                                                                                        MIC,
-       #                                                                                                        total_drop_nr,
-        #                                                                                                       new_volume,
-        #                                                                                                       loading,
-        #                                                                                                       growth)
-      #         , 'ABconc{}_loading{}_growth{}_vol{}.csv'.format(AB_conc, loading, growth, new_volume),'Time_list.csv', AB_conc) #, Nsat, total_drop_nr, loading,
-                    # growth, initialN, new_volume, growthrate, dt)
-       # print("--- %s seconds ---" % (time.time() - start_time))
-        pd.DataFrame(new).to_csv('output/df_growth_{}_loading_{}_ABconc{}.csv'.format(growth, loading,AB_conc), index= None)
+                #Droplet_exp.save('initialN{}_totaldropnr{}_vol{}_loading{}_growth{}.csv'.format(initialN,new_nr_drops_total_mass,new_volume,loading,growth),
+                #                 'ABconc{}_loading{}_growth{}_vol{}.csv'.format(AB_conc, loading, growth, new_volume),'Time_list.csv', AB_conc) #, Nsat, total_drop_nr, loading,growth, initialN, new_volume, growthrate, dt)
+        #print("--- %s seconds ---" % (time.time() - start_time))
+        pd.DataFrame(new).to_csv('output/df_growth_{}_loading_{}_ABconc{}.csv'.format(growth, loading, AB_conc), index = None)
         #print('total_drop_nr', total_drop_nr)
 
     ## Test the survival fraction of droplets for different factors, ab conc and calculate the total mass
@@ -180,31 +171,35 @@ class DropTest(object):
             pd.DataFrame(df_total_mass).to_csv('output/df_growth_{}_nr_drops_{}_big_droplet_nr_{}.csv'.format(growth, total_drop_nr,nr_drops_total_mass), index=None)
 
     ## needs changing
-    def count_total_mass(self, abmin, abmax, step):
+    def count_total_mass(self, partmin, partmax, step):
         df_total_mass = pd.DataFrame()
-        # loop for variables
-        for i in range(abmin, abmax, step):
-            new_ab_conc = i
-            print('new ab conc', new_ab_conc)
-            strain_R = strain(initialN, growthrate, deathrate, MIC, dt, t_end, Nsat)
-            Droplet_exp = droplets_R(total_drop_nr, strain_R, new_ab_conc, dt, t_end)  # 0.5, 300
-            Droplet_exp.run(loading, growth)
+        # loop for different partitioning factors:
+        # loop for partitioning factors
+        for i in range(partmin, partmax, step):
+            for j in range(0, 2, 1):
+                new_i = 5 ** i * 2 ** j
+                new_nr_drops_total_mass = new_i
+                new_volume = variables.volume * new_nr_drops_total_mass
+                total_drop_nr = round(variables.total_drop_nr / new_nr_drops_total_mass)
+                print('total_droplets', total_drop_nr)
+                strain_R = strain(new_nr_drops_total_mass)
+                Droplet_exp = droplets_R(total_drop_nr, strain_R, AB_conc, new_volume)  # 0.5, 300
+                Droplet_exp.run(loading, growth)
 
-
-            Droplet_exp.countTotalMass(growth)
-            additional = Droplet_exp.total_mass
-            df_total_mass ['Total_mass, ab conc {}'.format(new_ab_conc)] = additional
-
-        df_total_mass.insert(loc=0, column='Time', value=Droplet_exp.time_list_array)
+                Droplet_exp.countTotalMass(growth)
+                additional = Droplet_exp.total_mass
+                df_total_mass['Total_mass, multiplying factor {}'.format(new_nr_drops_total_mass)] = additional
+                df_total_mass.insert(loc=0, column='Time', value=Droplet_exp.time_list_array)
 
         # pd.DataFrame(new).to_csv('output/df_growth_{}_count_survival.csv'.format(growth), index=None)
-        pd.DataFrame(df_total_mass).to_csv('output/df_growth_{}_nr_drops_{}_big_droplet_nr_{}.csv'.format(growth, total_drop_nr, nr_drops_total_mass), index= None)
+        pd.DataFrame(df_total_mass).to_csv('output/df_growth_{}_starting_nr_drops_{}.csv'.format(growth,variables.total_drop_nr), index = None)
         print("--- %s seconds ---" % (time.time() - start_time))
 
 simulate = DropTest()
 #simulate.run()
 #simulate.test_dt(0, 10, 1)
-simulate.test_surv_frac_diff_partitioning(0, 5, 1)
+#simulate.test_surv_frac_diff_partitioning(0, 5, 1)
+simulate.count_total_mass(0, 5, 1)
 #simulate.test_surv_frac_diff_ab_conc(abmin, abmax, step)
 #simulate.test_survival_big_droplet_diff_ab_conc(abmin,abmax,step,nr_drop_min,nr_drop_max,step_drop)
 #simulate.count_total_mass(abmin, abmax, step)
--- draft.py
+++ draft.py
@@ -1,1 +1,17 @@
-print(6e7/(6.022*1e23) * 350)
+new_i = 5**i * 2**j
+                new_nr_drops_total_mass = new_i
+                new_volume = variables.volume * new_nr_drops_total_mass
+                total_drop_nr = round(variables.total_drop_nr /new_nr_drops_total_mass)
+                strain_R = strain(new_nr_drops_total_mass)
+                Droplet_exp = droplets_R(total_drop_nr, strain_R, AB_conc, new_volume)  # 0.5, 300
+                Droplet_exp.run(loading, growth)
+                Droplet_exp.countSurvival(growth)
+                additional = pd.DataFrame({"" + str(new_nr_drops_total_mass) + "": [Droplet_exp.Res_survival_fraction]})
+                # print(additional)
+                new = pd.concat([new, additional], axis=1)
+                #Droplet_exp.plots(growth)
+                #Droplet_exp.save('initialN{}_totaldropnr{}_vol{}_loading{}_growth{}.csv'.format(initialN,new_nr_drops_total_mass,new_volume,loading,growth),
+                #                 'ABconc{}_loading{}_growth{}_vol{}.csv'.format(AB_conc, loading, growth, new_volume),'Time_list.csv', AB_conc) #, Nsat, total_drop_nr, loading,growth, initialN, new_volume, growthrate, dt)
+        #print("--- %s seconds ---" % (time.time() - start_time))
+        pd.DataFrame(new).to_csv('output/df_growth_{}_loading_{}_ABconc{}.csv'.format(growth, loading, AB_conc), index = None)
+        #print('total_drop_nr', total_drop_nr)
