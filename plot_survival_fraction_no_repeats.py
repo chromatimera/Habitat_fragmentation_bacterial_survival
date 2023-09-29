8e56a0574fcef0de95178c01eb3c0cdd0dba72e1
chromatimera <popescuiofelia@gmail.com>
2023-09-29 23:58:58 +0100

    plotting update

--- plot_survival_fraction_no_repeats.py
+++ plot_survival_fraction_no_repeats.py
@@ -4,6 +4,15 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
+BIGGER_SIZE = 22
+
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+plt.rc('text', usetex=True)
+plt.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
+
 print(os.getcwd())
 
 #g1 = 'binary'
@@ -12,14 +21,14 @@ nr_drops = 1000
 
 droplet_list = np.arange(0, 1001, 50)
 droplet_list[0] = 1
-antib = [15, 20, 25, 30, 35]
+antib = [10, 15, 20, 30]
 color = iter(plt.cm.rainbow(np.linspace(0, 1, 5)))
 
 m_list = []
 survival_outcomes = []
 color_list = []
 
-plt.figure(1)
+plt.figure(figsize=(8,7))
 
 for ab, c, ind in zip(antib, color, range(len(antib))):
     if ind == 2:
@@ -48,9 +57,10 @@ for ab, c, ind in zip(antib, color, range(len(antib))):
     df = pd.DataFrame(list_of_tuples, columns = ['M', 'Surv frac'])
     print(df)
 
+    df['Error95'] = df.apply(lambda x: 2 * math.sqrt(x['Surv frac'] * (1 - x['Surv frac']))/ math.sqrt(x['M']), axis=1)
+    df['Error99'] = df.apply(lambda x: 2.6 * math.sqrt(x['Surv frac'] * (1 - x['Surv frac']))/ math.sqrt(x['M']), axis=1)
     df = df.set_index('M')
-    df['Error95'] = df.apply(lambda x: 2 * math.sqrt(x['Surv frac'] * (1 - x['Surv frac']))/ math.sqrt(nr_drops), axis=1)
-    df['Error99'] = df.apply(lambda x: 2.6 * math.sqrt(x['Surv frac'] * (1 - x['Surv frac']))/ math.sqrt(nr_drops), axis=1)
+
     print(df)
 
 
@@ -58,8 +68,8 @@ for ab, c, ind in zip(antib, color, range(len(antib))):
     df["Surv frac"].plot.line(yerr = df['Error95'], c=c)
     m_list.clear()
     survival_outcomes.clear()
-plt.ylabel('Fraction of droplets surviving')
-plt.xlabel('M (number of subvolumes)')
-plt.legend(antib,loc='upper right', title='Growth')
-plt.savefig('./output/Survival fraction adaptive vs gillespie balanced g.png')
+plt.ylabel(r'\bf{Fraction of droplets surviving}')
+plt.xlabel(r'\bf{m (number of subvolumes)}')
+plt.legend(antib, title=r'\bf{Antibiotic concentration in $\mu$g/mL}', loc='upper center', bbox_to_anchor=(0.5, 1.18), ncol=4, fancybox=True, shadow=True, title_fontsize=BIGGER_SIZE-5)
+plt.savefig('./output/Survival fraction 1 simulation.png', dpi=600)
 plt.show()
